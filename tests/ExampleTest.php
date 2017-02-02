149bec7d2a370d87795b9fa9734d476ecb55298e
Hunter Skrasek <hunterskrasek@me.com>
2017-02-01 22:01:28 -0600

    Update the ExampleTest to pass on our home page
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic functional test example.
      *
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->see('We rage quit often.');
     }
 }
