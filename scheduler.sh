a47dd4f0fcb5d3cf0a8d17f5b449920078b71a48
Hunter Skrasek <hunterskrasek@me.com>
2017-02-07 21:04:10 -0600

    Update the scheduler script to start pulling down more data
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- scheduler.sh
+++ scheduler.sh
@@ -5,12 +5,12 @@ TODAY_LOG="scheduler_${TODAY}.log"
 YESTERDAY=$(date "+%Y-%d-%m" -d "yesterday")
 YESTERDAY_LOG="scheduler_${YESTERDAY}.log"
 
-if [ -f /home/forge/default/storage/logs/${YESTERDAY_LOG} ]; then
-    rm /home/forge/default/storage/logs/${YESTERDAY_LOG}
+if [ -f /home/forge/dashboardersheaven.com/storage/logs/${YESTERDAY_LOG} ]; then
+    rm /home/forge/dashboardersheaven.com/storage/logs/${YESTERDAY_LOG}
 fi
 
-if [ ! -f /home/forge/default/storage/logs/${TODAY_LOG} ]; then
-    touch /home/forge/default/storage/logs/${TODAY_LOG}
+if [ ! -f /home/forge/dashboardersheaven.com/storage/logs/${TODAY_LOG} ]; then
+    touch /home/forge/dashboardersheaven.com/storage/logs/${TODAY_LOG}
 fi
 
-php /home/forge/default/artisan schedule:run >> /home/forge/default/storage/logs/${TODAY_LOG}
+php /home/forge/dashboardersheaven.com/artisan schedule:run >> /home/forge/dashboardersheaven.com/storage/logs/${TODAY_LOG}
