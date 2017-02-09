94c060a667b67ff78f31f82206580eb6eb00da70
Hunter Skrasek <hunterskrasek@me.com>
2017-02-09 00:13:46 -0600

    Consider a clip to be expired if we no longer get a media URL for it.
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Console/Commands/UpdateGamersClipsCommand.php
+++ app/Console/Commands/UpdateGamersClipsCommand.php
@@ -124,6 +124,15 @@ class UpdateGamersClipsCommand extends Command
             ]);
         }
 
+        if (empty($clipData['url'])) {
+            unset($clipData['url']);
+            $clipData['expired'] = true;
+            \Log::notice('clip.expired', [
+                'clip_id' => $clipData['clip_id'],
+                'xuid'    => $clipData['xuid']
+            ]);
+        }
+
         Bugsnag::leaveBreadcrumb(
             'Processed clip data',
             \Bugsnag\Breadcrumbs\Breadcrumb::MANUAL_TYPE,
